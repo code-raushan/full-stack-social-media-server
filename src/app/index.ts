c6390a5ea71799c37c057b24965e98e8bcb77a79
Raushan Kumar <raushan.development@gmail.com>
2023-07-15 23:15:05 +0530

    backend graphql server setup

---
+++ src/app/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import { ApolloServer } from '@apollo/server';
+import {expressMiddleware} from '@apollo/server/express4';
+import bodyParser from 'body-parser';
+export async function initServer(){
+    const app = express();
+    app.use(bodyParser.json())
+    const graphqlServer = new ApolloServer({
+        typeDefs: `
+            type Query{
+                sayHello: String
+            }
+        
+        `,
+        resolvers: {
+            Query:{
+                sayHello: ()=>'Hello from the server'
+            }
+        }
+    })
+    // waiting for the graphql server to start
+    await graphqlServer.start();
+    app.use("/graphql", expressMiddleware(graphqlServer));
+    return app;
+}
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import {initServer} from './app';
+
+const PORT = 8000;
+
+async function init(){
+    const app = await initServer();
+
+    app.listen(PORT, ()=>{
+        console.log('server started at http://localhost:8000')
+    })
+}
+init();
