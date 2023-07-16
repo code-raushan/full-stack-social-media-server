efb0455a8083d9dc09cd3c2132bb419aa886902c
Raushan Kumar <raushan.development@gmail.com>
2023-07-16 15:01:32 +0530

    auth graphql

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,19 +2,24 @@ import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import {expressMiddleware} from '@apollo/server/express4';
 import bodyParser from 'body-parser';
+import { prismaClient } from '../client/db';
+import { User } from './user';
+
 export async function initServer(){
     const app = express();
     app.use(bodyParser.json())
+    
     const graphqlServer = new ApolloServer({
         typeDefs: `
-            type Query{
-                sayHello: String
-            }
-        
+        ${User.types}
+
+        type Query{
+            ${User.queries}
+        }      
         `,
         resolvers: {
-            Query:{
-                sayHello: ()=>'Hello from the server'
+            Query: {
+                ...User.resolvers.queries
             }
         }
     })
