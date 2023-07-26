67a73614894e34734762fdb3895f8a17096e02fc
Raushan Kumar <raushan.development@gmail.com>
2023-07-27 01:10:18 +0530

    getUserById implemented

--- src/app/user/resolvers.ts
+++ src/app/user/resolvers.ts
@@ -4,7 +4,7 @@ import { prismaClient } from "../../client/db";
 import JWTService from "../../services/jwt";
 import { GraphqlContext } from "../../interfaces";
 import { User } from "@prisma/client";
-interface Result{
+interface Result {
     iss?: string;
     nbf?: string;
     aud?: string;
@@ -26,20 +26,20 @@ interface Result{
 }
 
 const queries = {
-    verifyGoogleToken: async (parent: any, { token }: {token:string})=>{
+    verifyGoogleToken: async (parent: any, { token }: { token: string }) => {
         const googleToken = token;
         const googleOAuthURL = new URL("https://oauth2.googleapis.com/tokeninfo");
         googleOAuthURL.searchParams.set("id_token", googleToken);
         // making the request to the google oauth2 service to get the information about the user
-        const {data} = await axios.get<Result>(googleOAuthURL.toString(), {responseType: "json"});
-        
+        const { data } = await axios.get<Result>(googleOAuthURL.toString(), { responseType: "json" });
+
         const user = await prismaClient.user.findUnique({
-            where:{email: data.email}
+            where: { email: data.email }
         });
         //if user does not exist in the database we will create the user in the db;
-        if(!user){
+        if (!user) {
             await prismaClient.user.create({
-                data:{
+                data: {
                     email: data.email,
                     firstName: data.given_name,
                     lastName: data.family_name,
@@ -48,31 +48,34 @@ const queries = {
             })
         }
         // checking for unique user in the db and handling errors;
-        const userInDB = await prismaClient.user.findUnique({where: {email: data.email}});
-        if(!userInDB) throw new Error('No user with this email exist');
-        
+        const userInDB = await prismaClient.user.findUnique({ where: { email: data.email } });
+        if (!userInDB) throw new Error('No user with this email exist');
+
         // generating the JWT token and returning it;
         const jwtToken = JWTService.generateJWT(userInDB);
         return jwtToken;
-        
+
     },
     // context is available for all the resolvers
-    getCurrentUser: async(parent:any, args:any, ctx: GraphqlContext)=>{
+    getCurrentUser: async (parent: any, args: any, ctx: GraphqlContext) => {
         // console.log(ctx.user);
         const id = ctx.user?.id;
-        const user = await prismaClient.user.findUnique({where: {
-            id
-        }});
+        const user = await prismaClient.user.findUnique({
+            where: {
+                id
+            }
+        });
         // console.log(user);
         return user;
-    }
+    },
+    getUserById: async (parent: any, { id }: { id: string }, ctx: GraphqlContext) => prismaClient.user.findUnique({ where: { id } })
 }
 
 // resolver to retrieve the posts mades the user
 const extraResolvers = {
     User: {
-        posts: (parent: User)=>
-            prismaClient.post.findMany({where: {author: {id: parent.id}}})       
+        posts: (parent: User) =>
+            prismaClient.post.findMany({ where: { author: { id: parent.id } } })
     }
 }
 
